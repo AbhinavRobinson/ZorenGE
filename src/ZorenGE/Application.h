d67c676cc724cdc6aad097c48f82e36aa6ad5534
Abhinav Robinson <abhinavrobinson@gmail.com>
2021-01-06 19:30:22 +0530

    (feat): add DLL configs

---
+++ src/ZorenGE/Application.h
@@ -0,0 +1,16 @@
+#pragma once
+	
+#include "Core.h"
+
+namespace ZorenGE {
+
+	class ZORENGE_API Application
+	{
+	public:
+		Application();
+		virtual ~Application();
+	
+		void Run();
+	};
+
+}
---
+++ src/ZorenGE/Core.h
@@ -0,0 +1,11 @@
+#pragma once
+
+#ifdef ZGE_PLATFORM_WINDOWS
+	#ifdef ZGE_BUILD_DLL
+		#define ZORENGE_API __declspec(dllexport)
+	#else
+		#define ZORENGE_API __declspec(dllimport)
+	#endif
+#else
+	#error Zoren is only supported on Windows!
+#endif
